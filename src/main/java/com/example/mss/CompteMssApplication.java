ef70ee1b17a349fad3e40456a8c8c8fe42d2842b
yasser1dev <yassirchihab1@gmail.com>
2021-02-12 16:56:21 +0100

    Expose Ids compte et opération

--- src/main/java/com/example/mss/CompteMssApplication.java
+++ src/main/java/com/example/mss/CompteMssApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -23,8 +24,10 @@ public class CompteMssApplication {
     }
 
     @Bean
-    CommandLineRunner start(CompteRepository compteRepository, OperationRepository operationRepository,RestRepoClient restRepoClient){
+    CommandLineRunner start(CompteRepository compteRepository, OperationRepository operationRepository,RestRepoClient restRepoClient,
+                            RepositoryRestConfiguration repositoryRestConfigurer){
         return args -> {
+            repositoryRestConfigurer.exposeIdsFor(Operation.class,Compte.class);
             Client client=restRepoClient.getClientById(1L);
             Compte compte=compteRepository.save(new Compte(null,10000.0,new Date(),client.getCode(),client,
                     TypeCompte.COURANT,Etat.ACTIVE,null));
